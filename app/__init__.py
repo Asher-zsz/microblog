ec7c1c110b1fada777c876542ada228985a7af7c
Asher <asher_zsz@163.com>
2022-04-11 19:59:09 +0800

    User Login

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 # app.config['SECRET_KEY'] = 'you-will-never-guess'
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login' # to tell Flask-Login which view function handles logins, 
+                           # so it can use @login_required to pretect certain views from anonymous users,
+                           # when these views are accessed, the decorator will redirect users to login_view
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
 from datetime import datetime
-from app import db
+from app import db, login
 
-class User(db.Model):
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model): # 通过继承UserMixin来继承 is_authenticated, is_active, is_anonymous, get_id()等几个属性与方法
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index = True, unique=True)
     email = db.Column(db.String(120), index = True, unique=True)
@@ -10,10 +13,16 @@ class User(db.Model):
     # backref='author' means to add a 'author' field to Post, and can be used like this：
     # >>> u = User.query.get(1)
     # >>> p = Post(body='my first post!', author=u)
+    
+    # methods to deal with password hash
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-    def __rep__(self):
-        return '<User {}>'.format(self.username) # the representation when printing
+    def __rep__(self): # the representation when printing
+        return '<User {}>'.format(self.username) 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,3 +32,7 @@ class Post(db.Model):
 
     def __rep__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
